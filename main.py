2c63657d08627a4e7b3153adfa7f049ac9ae0262
Blanka B <patryk.fruzynski@gmail.com>
2021-10-17 19:45:36 +0200

    formated output to dictionary

--- main.py
+++ main.py
@@ -4,27 +4,30 @@ from bs4 import BeautifulSoup
 
 
 URL = "https://mobile4ugsm.pl/pl/new/"
-url_4links = "https://mobile4ugsm.pl"
 page = requests.get(URL)
 
 soup = BeautifulSoup(page.content, "html.parser")
 job_elements = soup.find_all("div", class_="product s-grid-3 product-main-wrap")
+numberpgesprep = soup.find("ul", class_="paginator")
+z = max([s for s in re.findall('[0-9,]', numberpgesprep.text.strip())])
 
-for index, job_element in enumerate(job_elements):
-    name_element = job_element.find("span", class_="productname")
-    price_element = job_element.find("div", class_="price")
-    link_element = job_element.find("a", class_= "prodname")
-    name = name_element.text.strip()
-    price = price_element.text.strip()
-    p = "".join([s for s in re.findall('[0-9,]', price)])
-    print(p)
-    link = url_4links + link_element.get('href')
-    fin_object = {
-        'url': link,
-        'name': name,
-        'price': p + "zł"
-        }
+for i in range(int(z)):
+    URL = "https://mobile4ugsm.pl/pl/new/" + str(i+1)
+    url_4links = "https://mobile4ugsm.pl"
+    page = requests.get(URL)
+
+    soup = BeautifulSoup(page.content, "html.parser")
+    job_elements = soup.find_all("div", class_="product s-grid-3 product-main-wrap")
+    fin_object = {}
+    for index, job_element in enumerate(job_elements):
+        name_element = job_element.find("span", class_="productname")
+        price_element = job_element.find("div", class_="price")
+        link_element = job_element.find("a", class_= "prodname")
+        name = name_element.text.strip()
+        price = price_element.text.strip()
+        p = "".join([s for s in re.findall('[0-9,]', price)])
+        link = url_4links + link_element.get('href')
+        if link not in fin_object:
+            fin_object[link] = [name, p + "zł"]
     print(fin_object)
-
-
 
